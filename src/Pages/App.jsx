02276433e33e5b02a543cb6ea4fb1132c2e4e9f9
Kalpit Jain <kalpitjain@Kalpits-MacBook-Air.local>
2023-06-07 10:42:11 +0530

    inital commit

---
+++ src/Pages/App.jsx
@@ -0,0 +1,191 @@
+import React, { useState, useEffect } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import Contract from "../Contract.json";
+import FlipCard, { BackCard, FrontCard } from "../components/Flipcard";
+import { ethers } from "ethers";
+
+function App() {
+  const [mounted, setMounted] = useState(false);
+  const [isMinted, setIsMinted] = useState(false);
+  const [isMintLoading, setIsMintLoading] = useState(false);
+  const [isMintStarted, setIsMintStarted] = useState(false);
+  const [mintHash, setMintHash] = useState("");
+  useEffect(() => {
+    setMounted(true);
+    isConnected && getTotalSupply();
+  }, []);
+
+  const [totalMinted, setTotalMinted] = useState(0n);
+  const { isConnected, address } = useAccount();
+
+  async function mint(mintAmount) {
+    setIsMintLoading(true);
+    let appContract;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const chainId = await provider
+      .getNetwork()
+      .then((network) => network.chainId);
+
+    if (chainId === 80001) {
+      appContract = {
+        contractAddress: Contract.MumbaiContractAddress,
+        contractAbi: Contract.abi,
+      };
+    } else if (chainId === 97) {
+      appContract = {
+        contractAddress: Contract.BNBContractAddress,
+        contractAbi: Contract.abi,
+      };
+    } else if (chainId === 5) {
+      appContract = {
+        contractAddress: Contract.GoerliContractAddresss,
+        contractAbi: Contract.abi,
+      };
+    }
+
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(
+      appContract.contractAddress,
+      appContract.contractAbi,
+      signer
+    );
+
+    const tx = await contract.mint(mintAmount, {
+      gasLimit: 300000,
+    });
+
+    setIsMintLoading(false);
+    setIsMintStarted(true);
+
+    await tx.wait();
+
+    setIsMintStarted(true);
+    setIsMinted(true);
+
+    setMintHash(tx.hash);
+
+    if (tx.type === 2) {
+      setTotalMinted((prev) => {
+        return prev.toNumber() + 1;
+      });
+    } else {
+      window.location.reload();
+    }
+  }
+
+  async function getTotalSupply() {
+    let appContract;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const chainId = await provider
+      .getNetwork()
+      .then((network) => network.chainId);
+
+    if (chainId === 80001) {
+      appContract = {
+        contractAddress: Contract.MumbaiContractAddress,
+        contractAbi: Contract.abi,
+      };
+    } else if (chainId === 97) {
+      appContract = {
+        contractAddress: Contract.BNBContractAddress,
+        contractAbi: Contract.abi,
+      };
+    } else {
+      appContract = {
+        contractAddress: Contract.GoerliContractAddresss,
+        contractAbi: Contract.abi,
+      };
+    }
+
+    const contract = new ethers.Contract(
+      appContract.contractAddress,
+      appContract.contractAbi,
+      provider
+    );
+
+    const supply = await contract.totalSupply();
+
+    setTotalMinted(supply);
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div style={{ flex: "1 1 auto" }}>
+          <div style={{ padding: "24px 24px 24px 0" }}>
+            <h1>Project Management NFT</h1>
+            <p style={{ margin: "12px 0 24px" }}>
+              {Number(totalMinted)} minted so far!
+            </p>
+            <ConnectButton showBalance={false} chainStatus="icon" />
+
+            {mounted && isConnected && !isMinted && (
+              <button
+                style={{ marginTop: 24 }}
+                disabled={!mint || isMintLoading || isMintStarted}
+                className="button"
+                data-mint-loading={isMintLoading}
+                data-mint-started={isMintStarted}
+                onClick={() => mint(1)}
+              >
+                {isMintLoading && "Waiting for approval"}
+                {isMintStarted && "Minting..."}
+                {!isMintLoading && !isMintStarted && "Mint"}
+              </button>
+            )}
+          </div>
+        </div>
+
+        <div style={{ flex: "0 0 auto" }}>
+          <FlipCard>
+            <FrontCard isCardFlipped={isMinted}>
+              <img
+                layout="responsive"
+                src="../nft.png"
+                width={300}
+                height={300}
+                alt="RainbowKit Demo NFT"
+              />
+            </FrontCard>
+
+            <BackCard isCardFlipped={isMinted}>
+              <div style={{ padding: 24 }}>
+                <img
+                  src="../nft.png"
+                  width={80}
+                  height={80}
+                  alt="RainbowKit Demo NFT"
+                  style={{ borderRadius: 8 }}
+                />
+                <h2 style={{ marginTop: 24, marginBottom: 6 }}>NFT Minted!</h2>
+                <p style={{ marginBottom: 24 }}>
+                  Your NFT will show up in your wallet in the next few minutes.
+                </p>
+                <p style={{ marginBottom: 6 }}>
+                  View on{" "}
+                  <a href={`https://mumbai.polygonscan.com/tx/${mintHash}`}>
+                    Polygonscan
+                  </a>
+                </p>
+                <p>
+                  View on{" "}
+                  <a
+                    href={`https://testnets.opensea.io/assets/polygon/${address?.to}/1`}
+                  >
+                    Opensea
+                  </a>
+                </p>
+              </div>
+            </BackCard>
+          </FlipCard>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Hello from "./Pages/Hello";
+import App from "./Pages/App";
+import "@rainbow-me/rainbowkit/styles.css";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { polygonMumbai, bscTestnet, goerli } from "wagmi/chains";
+import {
+  metaMaskWallet,
+  walletConnectWallet,
+  coinbaseWallet,
+  trustWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const { chains, provider } = configureChains(
+  [polygonMumbai, goerli, bscTestnet],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({ http: chain.rpcUrls.default.http[0] }),
+    }),
+  ]
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const connectors = connectorsForWallets([
+  {
+    groupName: "Popular",
+    wallets: [
+      metaMaskWallet({ chains }),
+      walletConnectWallet({ chains }),
+      coinbaseWallet({ chains }),
+      trustWallet({ chains }),
+    ],
+  },
+]);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider modalSize="compact" chains={chains}>
+        <Router>
+          <Routes>
+            <Route path="/hello" element={<Hello />} />
+            <Route path="/" element={<App />} />
+          </Routes>
+        </Router>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
